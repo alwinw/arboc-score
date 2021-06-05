b3bcf004d82256bf1376925c2dab71d6576b154f
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-06-05 21:18:38 +1000

    Updated health check url

--- shiny_arboc-score/app.R
+++ shiny_arboc-score/app.R
@@ -1,19 +1,9 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("AKI risk based on creatinine (ARBOC) score"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
